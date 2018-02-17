6cf2c2cf30e0d82024a55e44aaeafe702ec6aaa3
Cesar Flores <flores.cesar.j@gmail.com>
2018-02-16 20:14:07 -0800

    Updated a note saying roman-to-int project was successful.

--- roman-to-integer/leetcode/Solution.java
+++ roman-to-integer/leetcode/Solution.java
@@ -3,6 +3,10 @@ Given a roman numeral, convert it to an integer.
 Input is guaranteed to be within the range from 1 to 3999.
 */
 
+/* This answer was accepted.
+ *
+ */
+
 package leetcode;
 
 class Solution{
