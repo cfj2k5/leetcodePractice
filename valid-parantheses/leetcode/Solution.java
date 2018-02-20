bb2ed28753540cd57a2ea72d8766270336a01f6b
Cesar Flores <flores.cesar.j@gmail.com>
2018-02-19 21:28:08 -0800

    Added solution and lessons learned to make the function more efficient.

--- valid-parantheses/leetcode/Solution.java
+++ valid-parantheses/leetcode/Solution.java
@@ -11,82 +11,109 @@ The brackets must close in the correct order, "()" and "()[]{}" are all valid bu
  * kind of bracket? Does it have to be an openning bracket? I'll look into this a bit more later. Got to work now.
  *
  */
+
+/* Re-read the question and adjusted my assumptions.
+ *
+ * This solution works. It aligns with many in the discussion, but I could have simplified it a bit.
+ *
+ */
 package leetcode;
+
+import java.util.*;
 
 class Solution{
 
 	public static void main(String[] args){
-		System.out.println("The string " + args[0] + " is valid: " + isValid(args[0]));	
+		System.out.println("The string " + args[0] + " isValid(): " + isValid(args[0]));	
+		System.out.println("The string " + args[0] + " isValid2(): " + isValid2(args[0]));	
 	}
 
 	public static boolean isValid(String s){
-       /* array to hold an accounting of brackets */
+        /* array to hold an accounting of brackets */
         /* in this order: ( ), { }, [ ] */
-        int[] counter = {0, 0, 0};
+        LinkedList<Character> fifo = new LinkedList<Character>();
+        
         for(int i = 0; i < s.length(); i++){
             switch(s.charAt(i)){
                 case '(':
-                    counter[0] += 1;
+                    fifo.push('(');
                     break;
                 case ')':
-                    if(counter[0] == 0){
+                    if(fifo.size() == 0){
                          /* edge case, already know string is unpaired */
                         return false;
-		    }
-				
-		    if(i > 0){
-			    /* edge case that was given, brackets must close in order */
-			if(s.charAt(i-1) != '('){
-				return false;
-			}
-		    }
-                    counter[0] -= 1;
+		            }
+                    if(fifo.peek() != '('){
+                        /* edge case, not closed in order */
+                        return false;
+                    }
+                    fifo.pop(); /* pop the ( */
                     break;
                 case '{':
-                    counter[1] += 1;
+                    fifo.push('{');
                     break;
                 case '}':
-                    if(counter[1] == 0){
-                        /* edge case, already know this is unpaired */
+                    if(fifo.size() == 0){
+                         /* edge case, already know string is unpaired */
+                        return false;
+		            }
+                    if(fifo.peek() != '{'){
+                        /* edge case, not closed in order */
                         return false;
                     }
-		    if(i > 0){
-			/* edge case where they must close in order */
-			    if(s.charAt(i-1) != '{'){
-				return false;
-			    }
-		    }
-                    counter[1] -= 1;
+                    fifo.pop(); /* pop the { */
                     break;
                 case '[':
-                    counter[2] += 1;
+                    fifo.push('[');
                     break;
                 case ']':
-                    if(counter[2] == 0){
-                        /* edge case, already know this is unpaired */
+                    if(fifo.size() == 0){
+                         /* edge case, already know string is unpaired */
+                        return false;
+		            }
+                    if(fifo.peek() != '['){
+                        /* edge case, not closed in order */
                         return false;
                     }
-		    if(i > 0){
-			/* edge case where the must close in order */
-			    if(s.charAt(i-1) != '['){
-				return false;
-			    }
-		    }
-                    counter[2] -= 1;
+                    fifo.pop(); /* pop the [ */
                     break;
                 default:
                     break;
             }/* end switch */
         }/* end if */
+        
         /* check to see if we have any brackets unaccounted for */
-        for(int i = 0; i < 3; i++){
-            if(counter[i] != 0){
-                return false;
-            }
+        if(fifo.size() != 0){
+            return false;
         }
         return true;
-
 	}
-
-
+	public static boolean isValid2(String s){
+		LinkedList<Character> fifo = new LinkedList<Character>();
+		for(char c : s.toCharArray()){
+			switch(c){
+				/* similar to the above solution, we push a matching paranthesis
+				 * but here, we push its opposite then check if the order is correct
+				 * and pop it when it is encountered. Saves a few operations and
+				 * we check mulitple edge cases at once.
+				 */
+				case '{':
+					fifo.push('}');
+					break;
+				case '(':
+					fifo.push(')');
+					break;
+				case '[':
+					fifo.push(']');
+					break;
+				default:
+					if((fifo.size() == 0) || (fifo.pop() != c)){
+						return false;
+					}
+				break;
+			}
+		}
+		
+		return fifo.isEmpty();
+	}/* isEmpty2 */
 }
